128d14507048255cabeb45778cffc5cb3374207b
Hamidreza-ygh <hr.yaghoubi@gmail.com>
2022-11-04 18:17:14 +0100

    V_4Nov_2

--- src/views/RequestPage.vue
+++ src/views/RequestPage.vue
@@ -1,34 +1,14 @@
 <template>
   <div class="bg-white dark:bg-gray-900">
    <FormComponent></FormComponent>
-   <CardComponent :data="products"></CardComponent>
   </div>
 </template>
 
-<script setup>
-import img1 from '/Users/hamidreza/Projects/Munda-Vue-static/munda/src/assets/image.jpg'
-const products = [
-  {
-    CarID: 1,
-    name: 'Land Rover Range Rover Sport',
-    href: '1022',
-    Image1: [{imageSrc: img1,}],
-    imageAlt: "",
-    PriceSelling: 59900,
-    color: 'Red',
-  },
-  
-  // More products...
-]
-</script>
-
-
 <script>
   import FormComponent from '@/components/FormComponent.vue';
-  import CardComponent from '@/components/CardComponent.vue';
   export default {
     name: 'RequestPage',
-    components: { FormComponent, CardComponent },
+    components: { FormComponent, },
     props: [],
     emits: [],
     methods: {
